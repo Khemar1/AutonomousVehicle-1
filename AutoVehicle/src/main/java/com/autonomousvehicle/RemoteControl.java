26a778c06f70d7089b1af71b6964c081c7fb6b13
Khemar1 <khemarbryan@gmail.com>
2016-10-17 17:41:00 -0400

    Added joystick view

--- AutoVehicle/src/main/java/com/autonomousvehicle/RemoteControl.java
+++ AutoVehicle/src/main/java/com/autonomousvehicle/RemoteControl.java
@@ -1,13 +1,22 @@
 package com.autonomousvehicle;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RemoteControl extends AppCompatActivity {
 
+    ActionBar actionBar = getActionBar();
+
+    private JoystickView joyStickCanvas;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remote_control);
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //joyStickCanvas = (JoystickView) findViewById(R.id.joystick);
     }
 }
