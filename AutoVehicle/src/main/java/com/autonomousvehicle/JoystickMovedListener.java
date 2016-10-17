caaf67a9652b16d16a685e52d0d6d68914726767
Khemar1 <khemarbryan@gmail.com>
2016-10-17 18:28:42 -0400

    made changes to joystick

---
+++ AutoVehicle/src/main/java/com/autonomousvehicle/JoystickMovedListener.java
@@ -0,0 +1,13 @@
+package com.autonomousvehicle;
+
+/**
+ * Created by Khemar on 2016-10-17.
+ */
+
+public interface JoystickMovedListener {
+
+        public void OnMoved(int pan, int tilt);
+
+        public void OnReleased();
+}
+
