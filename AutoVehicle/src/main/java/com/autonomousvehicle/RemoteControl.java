f56fc51317d0e6658b132867f30a9d697144207b
Khemar1 <khemarbryan@gmail.com>
2016-11-10 00:48:32 -0500

    Added a working Joystick

--- AutoVehicle/src/main/java/com/autonomousvehicle/RemoteControl.java
+++ AutoVehicle/src/main/java/com/autonomousvehicle/RemoteControl.java
@@ -10,12 +10,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class RemoteControl extends AppCompatActivity {
 
-   // ActionBar actionBar = getActionBar();
+    RelativeLayout layout_joystick;
+    ImageView image_joystick, image_border;
+    TextView textView1, textView2, textView3, textView4, textView5;
 
-  //  private JoystickView joyStickCanvas;
+    JoyStick js;
 
 
     @Override
@@ -24,7 +31,65 @@ public class RemoteControl extends AppCompatActivity {
         setContentView(R.layout.activity_remote_control);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-       // joyStickCanvas = (JoystickView) findViewById(R.id.joystick);
+
+        textView1 = (TextView)findViewById(R.id.textView1);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView4 = (TextView)findViewById(R.id.textView4);
+        textView5 = (TextView)findViewById(R.id.textView5);
+
+        layout_joystick = (RelativeLayout)findViewById(R.id.layout_joystick);
+
+        js = new JoyStick(getApplicationContext()
+                , layout_joystick, R.drawable.image_button);
+        js.setStickSize(150, 150);
+        js.setLayoutSize(500, 500);
+        js.setLayoutAlpha(150);
+        js.setStickAlpha(100);
+        js.setOffset(90);
+        js.setMinimumDistance(50);
+
+        layout_joystick.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                js.drawStick(arg1);
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN
+                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+                    textView1.setText("X : " + String.valueOf(js.getX()));
+                    textView2.setText("Y : " + String.valueOf(js.getY()));
+                    textView3.setText("Angle : " + String.valueOf(js.getAngle()));
+                    textView4.setText("Distance : " + String.valueOf(js.getDistance()));
+
+                    int direction = js.get8Direction();
+                    if(direction == JoyStick.STICK_UP) {
+                        textView5.setText("Direction : Up");
+                    } else if(direction == JoyStick.STICK_UPRIGHT) {
+                        textView5.setText("Direction : Up Right");
+                    } else if(direction == JoyStick.STICK_RIGHT) {
+                        textView5.setText("Direction : Right");
+                    } else if(direction == JoyStick.STICK_DOWNRIGHT) {
+                        textView5.setText("Direction : Down Right");
+                    } else if(direction == JoyStick.STICK_DOWN) {
+                        textView5.setText("Direction : Down");
+                    } else if(direction == JoyStick.STICK_DOWNLEFT) {
+                        textView5.setText("Direction : Down Left");
+                    } else if(direction == JoyStick.STICK_LEFT) {
+                        textView5.setText("Direction : Left");
+                    } else if(direction == JoyStick.STICK_UPLEFT) {
+                        textView5.setText("Direction : Up Left");
+                    } else if(direction == JoyStick.STICK_NONE) {
+                        textView5.setText("Direction : Center");
+                    }
+                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
+                    textView1.setText(R.string.x);
+                    textView2.setText(R.string.y);
+                    textView3.setText(R.string.angle);
+                    textView4.setText(R.string.distance);
+                    textView5.setText(R.string.direction);
+                }
+                return true;
+            }
+        });
+
     }
 
     @Override
