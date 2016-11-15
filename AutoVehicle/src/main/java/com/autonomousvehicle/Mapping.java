bfe0d3787d49c7b066d7b80a2f0a1205b7807159
Khemar1 <khemarbryan@gmail.com>
2016-11-15 16:12:31 -0500

    Added stopwatch for mapping/autonomous feature

--- AutoVehicle/src/main/java/com/autonomousvehicle/Mapping.java
+++ AutoVehicle/src/main/java/com/autonomousvehicle/Mapping.java
@@ -7,8 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 public class Mapping extends AppCompatActivity {
+
+    Chronometer mChronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,31 @@ public class Mapping extends AppCompatActivity {
         setContentView(R.layout.activity_mapping);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button button;
+
+        mChronometer = (Chronometer) findViewById(R.id.chronometer);
+
+        button = (Button) findViewById(R.id.start);
+        button.setOnClickListener(mStartListener);
+
+        button = (Button) findViewById(R.id.stop);
+        button.setOnClickListener(mStopListener);
+
     }
 
 
+    View.OnClickListener mStartListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            mChronometer.start();
+        }
+    };
+
+    View.OnClickListener mStopListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            mChronometer.stop();
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
